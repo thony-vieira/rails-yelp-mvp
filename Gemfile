79106f6c935e1493bfb603b5c8561817a75d898a
anthony vieira <thony16@gmail.com>
2023-11-16 10:34:29 -0300

    index completo

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source "https://rubygems.org"
 
+gem "simple_form"
+
+gem 'faker'
+
 ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create!([{
+  name:'trinta e cincão',
+  address: 'depois de umas ladeiras',
+  category: 'belgian'
+},
+{
+  name:'Spoleto',
+  address: 'Qualquer shopping',
+  category: 'italian'
+},
+{
+  name:'china in box',
+  address: 'qualquer esquina',
+  category: 'chinese'
+},
+{
+  name:'paris 6',
+  address: 'centro',
+  category: 'french'
+},
+  {name:    'La Nona di Lucca',
+   address: 'moema',
+   category: 'italian'
+}])
+
+p "Created #{Restaurant.count} restaurants"
